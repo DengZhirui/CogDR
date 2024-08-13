78a8d5d12d63fbc9be74cf1cd2c70adfed620869
Sylvia <38639102+DengZhirui@users.noreply.github.com>
2024-08-13 11:01:26 +0800

    Update embedding_initializing.sh

--- code/embedding_initializing.sh
+++ code/embedding_initializing.sh
@@ -1,5 +1,5 @@
 DATASET=$1
 
-python kg/kg_extractor.py --dataset $DATASET_my --mode read_item
-python kg/kg_extractor.py --dataset ml-1m_my --name ml-1m- --mode reset_index
-python kg/train.py -exp True -mn TransR -ds ml-1m -dsp ../datasets/ml-1m_my/kgdata/ -hpf kg/custom_hp.yaml > transr.out
+python emb_init/extractor.py --dataset ${DATASET}_my --mode read_item
+python emb_init/extractor.py --dataset ${DATASET}_my --name ${DATASET}- --mode reset_index
+python emb_init/train.py -exp True -mn TransR -ds ${DATASET} -dsp ../datasets/${DATASET}_my/kgdata/ -hpf kg/custom_hp.yaml
